1b1c9c07f24a8aab5f694d9d4cca43a09b38a89c
Vic <lolxdwiktoreeek@gmail.com>
2025-09-30 18:57:00 +0200

    Create password_creator.py

---
+++ password_creator.py
@@ -0,0 +1,26 @@
+import random
+
+# znaki których może użyć hasło
+chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890+-/*!&$#?=@"
+# pusta lista
+password_characters_list = []
+
+# funkcja tworząca te hasło
+def password_creator(all_chars, empty):
+    length = input("Podaj długość hasła.")
+    num = ""
+    while not num is int:
+        try:
+            num = int(length)
+            break
+        except:
+            print("Przepraszam, ale długość musi być liczbą.")
+            length = input("Podaj długość hasła.")
+    for i in range(num):
+        character = random.choice(all_chars)
+        empty.append(character)
+    password = "".join(empty)
+    print("Wygenerowane hasło:", password)
+
+# aktywacja funkcji
+password_creator(chars, password_characters_list)
