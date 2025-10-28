4917fcc5bf6d75ea67c88989945de056814f0496
Vic <lolxdwiktoreeek@gmail.com>
2025-10-28 18:56:16 +0100

    blud got sent

---
+++ discord_math_solver.py
@@ -0,0 +1,30 @@
+import discord
+import random 
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Logged on as {bot.user}')
+
+@bot.command()
+async def solve(ctx, expression):
+    try:
+        result = eval(expression)
+        try:
+            int(result)
+            await ctx.send(str(result))
+        except:
+            try:
+                float(result)
+                await ctx.send(str(result))
+            except:
+                await ctx.send(file=picture)
+    except:
+        await ctx.send(file=picture)
+
+bot.run("DISCORD BOT TOKEN")
